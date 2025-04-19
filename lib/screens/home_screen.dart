6afa746c1c0a0e40ff95511207827cffb5ae476c
Test <Test@brawlik.ru>
2025-04-19 19:11:55 +0300

    повиксил избранные)))

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/models/recipe.dart';
+import 'package:flutter_app/models/recipe_list.dart';
 import 'package:flutter_app/screens/add_recipe_screen.dart';
-// ignore: depend_on_referenced_packages
+import 'package:flutter_app/screens/recipe_detail.dart';
+import 'package:flutter_app/widgets/recipe_card.dart';
 import 'package:provider/provider.dart';
-import '../models/recipe_list.dart';
-import 'recipe_detail.dart';
+import '../models/theme_provider.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Настройки')),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            SwitchListTile(
+              title: Text('Темная тема'),
+              value: themeProvider.themeMode == ThemeMode.dark,
+              onChanged: (value) => themeProvider.toggleTheme(value),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -17,40 +40,19 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.star),
-            onPressed: () => Navigator.pushNamed(context, '/favorites'),
+            onPressed: () => _showFavorites(context, recipeList),
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed:
+                () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (ctx) => SettingsScreen()),
+                ),
           ),
         ],
       ),
-      body: FutureBuilder(
-        future: recipeList.loadRecipes(),
-        builder:
-            (ctx, snapshot) => GridView.builder(
-              padding: EdgeInsets.all(16),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.8,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-              ),
-              itemCount: recipeList.recipes.length,
-              itemBuilder:
-                  (ctx, index) => Dismissible(
-                    key: Key(recipeList.recipes[index].id),
-                    direction: DismissDirection.endToStart,
-                    background: Container(
-                      color: Colors.red,
-                      alignment: Alignment.centerRight,
-                      padding: EdgeInsets.only(right: 20),
-                      child: Icon(Icons.delete, color: Colors.white),
-                    ),
-                    onDismissed:
-                        (_) => recipeList.removeRecipe(
-                          recipeList.recipes[index].id,
-                        ),
-                    child: RecipeCard(recipe: recipeList.recipes[index]),
-                  ),
-            ),
-      ),
+      body: _buildRecipeGrid(recipeList),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed:
@@ -61,75 +63,59 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-}
 
-class RecipeCard extends StatelessWidget {
-  final Recipe recipe;
+  void _showFavorites(BuildContext context, RecipeList recipeList) {
+    final favoriteRecipes =
+        recipeList.recipes.where((r) => r.isFavorite).toList();
 
-  RecipeCard({required this.recipe});
-
-  @override
-  Widget build(BuildContext context) {
-    return Hero(
-      tag: recipe.id,
-      child: Card(
-        elevation: 4,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        child: InkWell(
-          onTap:
-              () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (ctx) => RecipeDetailScreen(recipe: recipe),
+    showModalBottomSheet(
+      context: context,
+      builder:
+          (ctx) => Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Text(
+                  'Избранное',
+                  style: Theme.of(context).textTheme.headlineLarge,
                 ),
-              ),
-          child: Column(
-            children: [
-              Expanded(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-                  child: Image.asset(
-                    recipe.imageUrl,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: favoriteRecipes.length,
+                    itemBuilder:
+                        (ctx, index) => ListTile(
+                          title: Text(favoriteRecipes[index].title),
+                          onTap:
+                              () => Navigator.push(
+                                ctx,
+                                MaterialPageRoute(
+                                  builder:
+                                      (ctx) => RecipeDetailScreen(
+                                        recipe: favoriteRecipes[index],
+                                      ),
+                                ),
+                              ),
+                        ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      recipe.title,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Row(
-                      children: [
-                        Chip(label: Text(recipe.category)),
-                        Spacer(),
-                        IconButton(
-                          icon: Icon(
-                            recipe.isFavorite
-                                ? Icons.favorite
-                                : Icons.favorite_border,
-                            color: Colors.red,
-                          ),
-                          onPressed:
-                              () => Provider.of<RecipeList>(
-                                context,
-                                listen: false,
-                              ).toggleFavorite(recipe.id),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
+    );
+  }
+
+  Widget _buildRecipeGrid(RecipeList recipeList) {
+    return GridView.builder(
+      padding: EdgeInsets.all(16),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.8,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
       ),
+      itemCount: recipeList.recipes.length,
+      itemBuilder:
+          (ctx, index) => RecipeCard(recipe: recipeList.recipes[index]),
     );
   }
 }
--- lib/widgets/recipe_card.dart
+++ lib/widgets/recipe_card.dart
@@ -1,1 +1,59 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+import 'package:flutter_app/screens/recipe_detail.dart';
+import 'package:provider/provider.dart';
+import '../models/recipe.dart';
+import '../models/recipe_list.dart';
+
+class RecipeCard extends StatelessWidget {
+  final Recipe recipe;
+
+  RecipeCard({required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    final recipeList = Provider.of<RecipeList>(context, listen: false);
+
+    return Hero(
+      tag: recipe.id,
+      child: Card(
+        child: InkWell(
+          onTap:
+              () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (ctx) => RecipeDetailScreen(recipe: recipe),
+                ),
+              ),
+          child: Column(
+            children: [
+              Expanded(
+                child: Image.asset(
+                  recipe.imageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    Expanded(child: Text(recipe.title)),
+                    IconButton(
+                      icon: Icon(
+                        recipe.isFavorite
+                            ? Icons.favorite
+                            : Icons.favorite_border,
+                        color: Colors.red,
+                      ),
+                      onPressed: () => recipeList.toggleFavorite(recipe.id),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
